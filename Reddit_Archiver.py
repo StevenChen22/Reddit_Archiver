eb32acd539607f167b39c08e2bc55e237778adea
Steven Chen <sc22@pdx.edu>
2014-08-18 14:57:08 -0700

    Imported praw and started get

--- Reddit_Archiver.py
+++ Reddit_Archiver.py
@@ -2,3 +2,17 @@
 # Uses PRAW to archive Reddit posts from u/CityofVancouver
 
 import praw
+
+user_agent = ("Reddit Archiver 1.0 by /u/CityofVancouver"\
+                "https://github.com/StevenChen22/Reddit_Archiver")
+
+r = praw.Reddit(user_agent=user_agent)
+
+user_name = "CityofVancouver"
+user = r.get_redditor(user_name)
+
+comments = user.get_comments()
+submitted = user.get_submitted()
+
+#for i in comments and submnitted:
+    # add it to a file?
