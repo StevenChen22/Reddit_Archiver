9ba44b98da56369b63095d0328eac42a896bb6a2
Steven Chen <sc22@pdx.edu>
2014-08-20 11:25:44 -0700

    Initialized 'already_archived' functionality in tester.

--- Reddit_Archiver.py
+++ Reddit_Archiver.py
@@ -4,24 +4,11 @@
 # Import Python Reddit API Wrapper (PRAW) and time
 import praw
 import time
-import datetime
 
 debug = False
 
 output_archive = open("archive.txt", "w")
-already_archived = open("already_archived", "r+")
-
-# Define print to external source function
-
-
-# def add_comment_to_file(c):
-# Submit Date
-# Subreddit
-# PermaLink to comment
-# PermaLink to thread
-# Text
-# Parent or child comments?
-# Points?
+already_archived = open("already_archived.txt", "r+")
 
 # Connect to Reddit and identify user and script
 user_agent = ("Reddit Archiver 1.0 by /u/CityofVancouver"\
@@ -31,41 +18,20 @@ r = praw.Reddit(user_agent)
 # Specify Redditor
 user_name = "CityofVancouver"
 user = r.get_redditor(user_name)
-
-# List of already archived submissions
 if debug == True:
     print("Logged in")
-
-# Get comments and submissions made by user
-comments = user.get_comments(limit = 25)
-if debug == True:
-    print("Got Comments")
-submitted = user.get_submitted(limit = 10)
-if debug == True:
-    print("Got Submissions")
 
 # While loop - beause it's a Bot
 while True:
     if debug == True:
         print("Started Loop")
 
-    # Check comments
-    for i in comments:
-        if debug == True:
-            print("Started comment loop")
-        if i.id not in already_archived:
-            # Add it to a file:
-            # Title
-            # Submit Date
-            # Subreddit
-            # praw.objects.Comment.permaLink
-            # Self text or URL
-            # Comments?
-            # Points?
-            already_archived.append(i.id)
-            #output_archive.write(i.selftext + "\n")
+    # Get submissions made by Redditor
+    submitted = user.get_submitted(limit = 10)
+    if debug == True:
+        print("Got Submissions")
 
-    # Check submitted posts
+    # Archive user submitted posts
     for j in submitted:
         if debug == True:
             print("Started submission loop")
@@ -74,8 +40,8 @@ while True:
             already_archived.write(j.id + "\n")
             # Get submission datetime
             timestamp = j.created_utc
-            dt = datetime.datetime.utcfromtimestamp(int(timestamp)).strftime('%Y-%m-%d %H:%M:%S')
-            # Write to archive
+            dt = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(int(timestamp)))
+            # Write to archive (should I add comments?)
             output_archive.write("Submission Date: \n" + dt + "\n\n")
             output_archive.write("Permalink: \n" + str(j.permalink) + "\n\n")
             output_archive.write("Subreddit: \n" + str(j.subreddit) + "\n\n")
@@ -87,10 +53,30 @@ while True:
             elif j.is_self == False:
                 output_archive.write("Link: \n" + str(j.url) + "\n\n")
 
+    # Get user submitted comments made by Redditor
+    comments = user.get_comments(limit = 25)
+    if debug == True:
+        print("Got Comments")
+
+    # Archive user submitted comments and the parent
+    for i in comments:
+        if debug == True:
+            print("Started comment loop")
+        if i.id not in already_archived:
+            already_archived.append(i.id)
+            # Submit Date
+            # Subreddit
+            # PermaLink to comment
+            # PermaLink to thread
+            # Text
+            # Parent or original post
+            # Points
+
     # Set frequency of run
     if debug == True:
         print("Finished loops!")
-
+    False
 #    time.sleep(86400)
 
+already_archived.close()
 output_archive.close()
--- praw_tester.py
+++ praw_tester.py
@@ -1,36 +1,68 @@
+# Copyright 2014(c) Steven Chen
+# This is a testing file for Reddit_Archive.py
+# Currently does not run on a while loop
+
+import time
 import praw
-import datetime
-import pprint
 output_archive = open("archive.txt", "w")
+already_archived = open("already_archived.txt", "r+")
+
 
 user_agent = ("/u/CityofVancouver testing praw")
 r = praw.Reddit(user_agent=user_agent)
 
 user_name = "CityofVancouverWA"
 user = r.get_redditor(user_name)
-submission = user.get_submitted(limit = 1)
-for i in submission:
-    output_archive.write("Submission Date: \n" + str(i.created_utc) + "\n\n")
-    output_archive.write("Permalink: \n" + str(i.permalink) + "\n\n")
-    output_archive.write("Subreddit: \n" + str(i.subreddit) + "\n\n")
-    output_archive.write("Post Score: \n" + str(i.score) + "\n\n")
-    output_archive.write("Title: \n" + str(i.title) + "\n\n")
+
+debug = False
+
+# Get submissions made by Redditor
+submitted = user.get_submitted(limit = 10)
+if debug == True:
+    print("Got Submissions")
+
+# Archive user submitted posts
+for j in submitted:
+    if debug == True:
+        print("Started submission loop")
+
+    if j.id not in already_archived:
+        already_archived.write(j.id + "\n")
+        # Get submission datetime
+        timestamp = j.created_utc
+        dt = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(int(timestamp)))
+        # Write to archive (should I add comments?)
+        output_archive.write("Submission Date: \n" + dt + "\n\n")
+        output_archive.write("Permalink: \n" + str(j.permalink) + "\n\n")
+        output_archive.write("Subreddit: \n" + str(j.subreddit) + "\n\n")
+        output_archive.write("Post Score: \n" + str(j.score) + "\n\n")
+        output_archive.write("Title: \n" + str(j.title) + "\n\n")
+        # See if self post or not and write self text body or link
+        if j.is_self == True:
+            output_archive.write("Body: \n" + str(j.selftext) + "\n\n")
+        elif j.is_self == False:
+            output_archive.write("Link: \n" + str(j.url) + "\n\n")
+
+# -------------------------
+
+# THIS IS WORKING FOR PULLING SUMBMISSIONS
+#submission = user.get_submitted(limit = 1)
+#for i in submission:
+#    timestamp = i.created_utc
+#    dt = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(int(timestamp)))
+    # Write to archive (should I add comments?)
+#    output_archive.write("Submission Date: \n" + dt + "\n\n")
+#    output_archive.write("Permalink: \n" + str(i.permalink) + "\n\n")
+#    output_archive.write("Subreddit: \n" + str(i.subreddit) + "\n\n")
+#    output_archive.write("Post Score: \n" + str(i.score) + "\n\n")
+#    output_archive.write("Title: \n" + str(i.title) + "\n\n")
     # See if self post or not and write self text body or link
-    if i.is_self == True:
-        output_archive.write("Body: \n" + str(i.selftext) + "\n\n")
-    elif i.is_self == False:
-        output_archive.write("Link: \n" + str(i.url) + "\n\n")
+#    if i.is_self == True:
+#        output_archive.write("Body: \n" + str(i.selftext) + "\n\n")
+#    elif i.is_self == False:
+#        output_archive.write("Link: \n" + str(i.url) + "\n\n")
 
-
-
-    #print(i.selftext)
-    #print("****THIS IS THE PERMALINK", i.permalink)
-    #output_archive.write(i.selftext + "\n")
-    #output_archive.write(i.permalink + "\n")
-    #print("THIS IS THE URL", i.url)
-    #timestamp = i.created_utc
-    #dt = datetime.datetime.utcfromtimestamp(int(timestamp)).strftime('%Y-%m-%d %H:%M:%S')
-    #print(dt)
+# --------------
 
 #submissions = r.get_subreddit('python').get_top(limit=1)
 #submission = next(submissions)
@@ -41,3 +73,4 @@ for i in submission:
 #    output_archive.write(i.permalink)
 
 output_archive.close()
+already_archived.close()
