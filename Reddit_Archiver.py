bcaac5403d5039d1f6a8e36d0e70b407b1e62700
Steven Chen <sc22@pdx.edu>
2014-08-20 22:47:52 -0700

    Fixed more  commenting

--- Reddit_Archiver.py
+++ Reddit_Archiver.py
@@ -24,6 +24,7 @@ submitted = user.get_submitted(limit = 5)
 comments = user.get_comments(limit = 5)
 
 def archive_check(n):
+    """Check to see if ID has been archived already"""
     found = False
     for i in archived_list:
         return i
@@ -31,10 +32,10 @@ def archive_check(n):
             found = True
     return found
 
-# While loop - beause it's a Bot and it needs to run 5ever!
+# Main while loop - beause it's a Bot and it needs to run 5ever!
 counter = 0
 while counter < 2:
-    # Build already in archive list
+    # Build 'already in archive' list
     archived_list = []
     already_archived = open("already_archived.txt", "r")
 
@@ -53,7 +54,7 @@ while counter < 2:
     if debug == True:
         print("Got Submissions")
 
-    # Archive user submitted posts to file
+    # Archive data from user submitted posts to file
     for j in submitted:
         if debug == True:
             print("Started submission loop")
@@ -63,7 +64,7 @@ while counter < 2:
             # Get submission datetime
             timestamp = j.created_utc
             dt = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(int(timestamp)))
-            # Write to archive (should I add comments?)
+            # Write to archive
             output_archive.write("Submission Date: \n" + dt + "\n\n")
             output_archive.write("Permalink: \n" + str(j.permalink) + "\n\n")
             output_archive.write("Subreddit: \n" + str(j.subreddit) + "\n\n")
@@ -82,7 +83,7 @@ while counter < 2:
     if debug == True:
         print("Got Comments")
 
-    # Archive user submitted comments and the parent to file
+    # Archive data from user submitted comments and the parent to file
     for i in comments:
         if debug == True:
             print("Started comment loop")
@@ -91,15 +92,15 @@ while counter < 2:
             # Get submission datetime
             timestamp = i.created_utc
             dt = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(int(timestamp)))
-            # Write to archive (should I add comments?)
+            # Write to archive
             output_archive.write("Submission Date: \n" + dt + "\n\n")
-            # Permalink to comment
+            # Permalink to comment, not original post
             output_archive.write("Permalink: \n" + str(i.permalink) + "\n\n")
             output_archive.write("Subreddit: \n" + str(i.subreddit) + "\n\n")
             output_archive.write("Post Score: \n" + str(i.score) + "\n\n")
             # Get the parent ID
             parent = r.get_info(thing_id=i.parent_id)
-            # Print the parent body if comment or selftext if submission
+            # Write the parent body if comment or selftext if submission
             if i.is_root == True:
                 output_archive.write("Post Parent: \n" + str(parent.selftext) + "\n\n")
             elif i.is_root == False:
